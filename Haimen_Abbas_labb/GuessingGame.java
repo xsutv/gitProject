9d852be83489846d42f5e47acfb04905388a950f
Haimen <haimenabbas@gmail.com>
2024-10-23 15:32:49 +0200

    Created a bashscript run_game.sh

---
+++ Haimen_Abbas_labb/GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+    public static void main(String[] args) {
+        
+        Guesser game = new Guesser(0, 20);
+
+        game.start();
+
+    }
+}
---
+++ run_game.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+NAME="Haimen_Abbas"
+SUFFIX="_labb"
+
+echo "$NAME"
+
+mkdir "$NAME$SUFFIX"
+
+for f in *.java
+do
+    cp -v "$f" "$NAME$SUFFIX"
+done
+
+cd Haimen_Abbas_labb/
+
+pwd
+
+echo "Compiling all java files.."
+
+javac *.java
+
+echo "Running game.."
+
+java GuessingGame
+
+echo "Game finished"
+
+for f in *.class
+do
+    rm "$f" "$NAME$SUFFIX"
+done
+
+ls 
